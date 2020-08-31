9605d30d35178def93701c4746c094fc6ad560cf
Gabrielius Laurinavičius <gabrielius.laurinavicius@gmail.com>
2020-08-31 13:17:21 +0300

    Move build loop

--- cypress/integration/WorkshopTests/calculator.spec.js
+++ cypress/integration/WorkshopTests/calculator.spec.js
@@ -8,107 +8,76 @@ builds.unshift('Prototype');
 
 beforeEach('Visit site before each test', () => {
     cy.visit('https://testsheepnz.github.io/BasicCalculator');
-})
+});
 
+builds.forEach((build) => {
+    describe(`Build ${build}`, () => {
+        beforeEach('Select build before each test', () =>
+            calculator.selectBuild(build));
 
-describe('Number addition', () => {
-    builds.forEach((build) => {
-        it(`can add -1.5 + 2.6 with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.add(-1.5, 2.6);
+        describe('Number addition', () => {
+            it(`can add -1.5 + 2.6`, () =>
+                calculator.add(-1.5, 2.6));
+
+            it(`can add -1.5 + 2.6 with integers only`, () =>
+                calculator.add(-1.5, 2.6, true));
+        });
+
+        describe('Number subtraction', () => {
+            it.only(`can subtract 2.5 - 5`, () =>
+                calculator.subtract(2.5, 5));
+
+            it.only(`can subtract 2.5 - 5 with integers only`, () =>
+                calculator.subtract(2.5, 5, true));
+        });
+
+        describe('Number multiplication', () => {
+            it(`can multiply 2.5 * 5`, () =>
+                calculator.multiply(2.5, 5));
+
+            it(`can multiply 2.5 * 5  with integers only`, () =>
+                calculator.multiply(2.5, 5, true));
+        });
+
+        describe('Number division', () => {
+            it(`can divide 2.5 / 5`, () =>
+                calculator.divide(2.5, 5));
+
+            it(`can divide 2.5 / 5  with integers only`, () =>
+                calculator.divide(2.5, 5, true));
+        });
+
+        describe('Input validation', () => {
+            it.only(`validates first input`, () =>
+                calculator.validateInput('asd', 2));
+
+            it.only(`validates second input`, () =>
+                calculator.validateInput(2, 'asd'));
+        });
+
+        describe('Clear button', () => {
+            it.only(`clears answer`, () => {
+                calculator.getClearButton().should('not.be.disabled');
+
+                calculator.getAnswer(1, 1);
+                calculator.getAnswerField().invoke('val').should('not.be.empty');
+                calculator.getClearButton().click();
+                calculator.getAnswerField().invoke('val').should('be.empty');
+            });
+        });
+
+        describe('String concatenation', () => {
+            it.only(`can concatenate strings 'qwe' and 'asd'`, () =>
+                calculator.concatenate('qwe', 'asd'));
+        });
+
+        describe('Division by zero', () => {
+            it.only(`can handle division by zero error`, () => {
+                calculator.selectOperation(Calculator.OPERATIONS.DIVIDE);
+
+                calculator.getAnswer(0, 0);
+                calculator.getErrorField().should('be.visible').contains('Divide by zero error!');
+            });
         });
     });
-})
-
-
-describe('Number subtraction', () => {
-    builds.forEach((build) => {
-        it.only(`can subtract 2.5 - 5 with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.subtract(2.5, 5);
-        });
-    });
-})
-
-
-describe('Number multiplication', () => {
-    builds.forEach((build) => {
-        it(`can multiply 2.5 * 5 with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.multiply(2.5, 5);
-        });
-    });
-})
-
-
-describe('Number division', () => {
-    builds.forEach((build) => {
-        it(`can divide 2.5 / 5 with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.divide(2.5, 5);
-        });
-    });
-})
-
-
-describe('Input validation', () => {
-    builds.forEach((build) => {
-        it.only(`validates first input with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.validateInput('asd', 2);
-        });
-
-        it.only(`validates second input with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.validateInput(2, 'asd');
-        });
-    });
-})
-
-
-describe('Clear button', () => {
-    builds.forEach((build) => {
-        it.only(`clears answer with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.getClearButton().should('not.be.disabled');
-
-            calculator.getAnswer(1, 1);
-            calculator.getAnswerField().invoke('val').should('not.be.empty');
-            calculator.getClearButton().click();
-            calculator.getAnswerField().invoke('val').should('be.empty');
-        });
-    });
-})
-
-
-describe('Integers only option', () => {
-    builds.forEach((build) => {
-        it(`respects integers only option with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.add(1.2, 5.7, true);
-        });
-    });
-})
-
-
-describe('String concatenation', () => {
-    builds.forEach((build) => {
-        it.only(`can concatenate strings 'qwe' and 'asd' with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.concatenate('qwe', 'asd');
-        });
-    });
-})
-
-
-describe('Division by zero', () => {
-    builds.forEach((build) => {
-        it.only(`can handle division by zero error with build ${build}`, () => {
-            calculator.selectBuild(build);
-            calculator.selectOperation(Calculator.OPERATIONS.DIVIDE);
-
-            calculator.getAnswer(0, 0);
-            calculator.getErrorField().should('be.visible').contains('Divide by zero error!');
-        });
-    });
-})
+});
