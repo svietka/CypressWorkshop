289077406b89219b53ecf8c986d371ab801007bf
Aistisl <aistisl@gmail.com>
2020-09-01 12:16:48 +0300

    Updated calculator.spec.js

--- cypress/integration/CalculatorHomework/calculator.spec.js
+++ cypress/integration/CalculatorHomework/calculator.spec.js
@@ -11,6 +11,52 @@ it("Check if there's heading that contains 'Basic Calculator' text", ()=>{
     cy.get(".intro-heading").contains("Basic Calculator", { matchCase: false })
 })
 
+describe(`Device tests. All required elements are visible`, () => {
+    it("1280x720", ()=>{
+        cy.viewport(1280, 720)
+        cy.get("#selectBuild").should("be.visible")
+        homepage.getnumber1field().should("be.visible")
+        homepage.getnumber2field().should("be.visible")
+        cy.get("#selectOperationDropdown").should("be.visible")
+        cy.get("#calculateButton").should("be.visible")
+        cy.get("#numberAnswerField").should("be.visible")
+        cy.get("#integerSelect").should("be.visible")
+        cy.get("#clearButton").should("be.visible")
+    })
+
+    it("360x640", ()=>{
+        cy.viewport(640,360)
+        cy.get("#selectBuild").should("be.visible")
+        homepage.getnumber1field().should("be.visible")
+        homepage.getnumber2field().should("be.visible")
+        cy.get("#selectOperationDropdown").should("be.visible")
+        cy.get("#calculateButton").should("be.visible")
+        cy.get("#numberAnswerField").should("be.visible")
+        cy.get("#integerSelect").should("be.visible")
+        cy.get("#clearButton").should("be.visible")
+    })
+
+    it.only("768x1366", ()=>{
+        cy.viewport(768,1366)
+        cy.get("#selectBuild").should("be.visible")
+        homepage.getnumber1field().should("be.visible")
+        homepage.getnumber2field().should("be.visible")
+        cy.get("#selectOperationDropdown").should("be.visible")
+        cy.get("#calculateButton").should("be.visible")
+        cy.get("#numberAnswerField").should("be.visible")
+        cy.get("#integerSelect").should("be.visible")
+        cy.get("#clearButton").should("be.visible")
+    })
+})
+
+it("Scroll down and up on the page", ()=>{
+    cy.get(".footer").scrollIntoView()
+    cy.wait(5000)
+    cy.get(".masthead").scrollIntoView()
+})
+
+
+
 var build = ['Prototype','1','2','3','4','5','6','7','8','9']
 
 build.forEach((item) => {
@@ -28,7 +74,20 @@ it("After reload all input fields should be empty", ()=>{
     homepage.getnumber2field().should('have.value', '')
 })
 
-it("Test if all required fields are visible", ()=>{
+it.only("Test if clear button deletes answer field and uncheks integer chekcbox", ()=>{
+    cy.get("#selectBuild").select(item)
+    homepage.getnumber1field().clear()
+    homepage.getnumber1field().type(1)
+    homepage.getnumber2field().clear()
+    homepage.getnumber2field().type(2)
+    cy.get("#selectOperationDropdown").select("Add")
+    homepage.ClickClaculateButton()
+    cy.get("#clearButton").click()
+    cy.get("#numberAnswerField").should('have.value', '')
+    cy.get("#integerSelect").should("not.be.checked")
+})
+
+it.only("Test if all required fields are visible", ()=>{
     cy.get("#selectBuild").select(item)
     cy.get("#selectBuild").should("be.visible")
     homepage.getnumber1field().should("be.visible")
@@ -43,7 +102,7 @@ it("Test if all required fields are visible", ()=>{
 it("Enter string in both number fields and test 'add' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type("one")
+    homepage.getnumber1field().type("one")
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type("two")
     cy.get("#selectOperationDropdown").select("Add")
@@ -54,7 +113,7 @@ it("Enter string in both number fields and test 'add' operation", ()=>{
 it("Enter numeric value in first number field and string in second number field, test 'add' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(1)
+    homepage.getnumber1field().type(1)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type("two")
     cy.get("#selectOperationDropdown").select("Add")
@@ -62,12 +121,12 @@ it("Enter numeric value in first number field and string in second number field,
     cy.get("#errorMsgField").contains('Number 2 is not a number')
 })
 
-it.only("Enter both numbers together with arithmetic operators and test 'add' operation", ()=>{
+it("Enter both numbers together with arithmetic operators and test 'add' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(1+)
+    homepage.getnumber1field().type("1+")
     homepage.getnumber2field().clear()
-    homepage.getnumber2field().type(*2)
+    homepage.getnumber2field().type("*2")
     cy.get("#selectOperationDropdown").select("Add")
     homepage.ClickClaculateButton()
     cy.get("#errorMsgField").contains('Number 1 is not a number')
@@ -76,7 +135,7 @@ it.only("Enter both numbers together with arithmetic operators and test 'add' op
 it("Enter integer numbers and test 'add' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(1)
+    homepage.getnumber1field().type(1)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Add")
@@ -87,7 +146,7 @@ it("Enter integer numbers and test 'add' operation", ()=>{
 it("Enter negative integer numbers and test 'add' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(-1)
+    homepage.getnumber1field().type(-1)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(-2)
     cy.get("#selectOperationDropdown").select("Add")
@@ -95,10 +154,10 @@ it("Enter negative integer numbers and test 'add' operation", ()=>{
     cy.get("#numberAnswerField").should('have.value', -3)
 })
 
-it("Enter two integers, one negative, one positive and test 'add' operation", ()=>{
+it.only("Enter two integers, one negative, one positive and test 'add' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(-1)
+    homepage.getnumber1field().type(-1)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Add")
@@ -106,10 +165,21 @@ it("Enter two integers, one negative, one positive and test 'add' operation", ()
     cy.get("#numberAnswerField").should('have.value', 1)
 })
 
+it("Enter two integers, one negative, one positive and test 'Concatenate' operation", ()=>{
+    cy.get("#selectBuild").select(item)
+    homepage.getnumber1field().clear()
+    homepage.getnumber1field().type(-1)
+    homepage.getnumber2field().clear()
+    homepage.getnumber2field().type(2)
+    cy.get("#selectOperationDropdown").select("Concatenate")
+    homepage.ClickClaculateButton()
+    cy.get("#numberAnswerField").should('have.value', -12)
+})
+
 it("Enter integer numbers and test 'Subtract' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(3)
+    homepage.getnumber1field().type(3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Subtract")
@@ -120,7 +190,7 @@ it("Enter integer numbers and test 'Subtract' operation", ()=>{
 it("Enter negative integer numbers and test 'Subtract' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(-3)
+    homepage.getnumber1field().type(-3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Subtract")
@@ -131,7 +201,7 @@ it("Enter negative integer numbers and test 'Subtract' operation", ()=>{
 it("Enter two integers, one negative, one positive and test 'Subtract' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(-3)
+    homepage.getnumber1field().type(-3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(-2)
     cy.get("#selectOperationDropdown").select("Subtract")
@@ -142,7 +212,7 @@ it("Enter two integers, one negative, one positive and test 'Subtract' operation
 it("Enter integer numbers and test 'Multiply' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(3)
+    homepage.getnumber1field().type(3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Multiply")
@@ -153,7 +223,7 @@ it("Enter integer numbers and test 'Multiply' operation", ()=>{
 it("Enter negative integer numbers and test 'Multiply' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(-3)
+    homepage.getnumber1field().type(-3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Multiply")
@@ -164,7 +234,7 @@ it("Enter negative integer numbers and test 'Multiply' operation", ()=>{
 it("Enter two integers, one negative, one positive and test 'Multiply' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(-3)
+    homepage.getnumber1field().type(-3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(-2)
     cy.get("#selectOperationDropdown").select("Multiply")
@@ -175,7 +245,7 @@ it("Enter two integers, one negative, one positive and test 'Multiply' operation
 it("Enter integer numbers and test 'Divide' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(3)
+    homepage.getnumber1field().type(3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Divide")
@@ -186,7 +256,7 @@ it("Enter integer numbers and test 'Divide' operation", ()=>{
 it("Enter negative integer numbers and test 'Divide' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(-3)
+    homepage.getnumber1field().type(-3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Divide")
@@ -197,7 +267,7 @@ it("Enter negative integer numbers and test 'Divide' operation", ()=>{
 it("Enter two integers, one negative, one positive and test 'Divide' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(-3)
+    homepage.getnumber1field().type(-3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(-2)
     cy.get("#selectOperationDropdown").select("Divide")
@@ -208,7 +278,7 @@ it("Enter two integers, one negative, one positive and test 'Divide' operation",
 it("Enter 0 divide it by integer number and test 'Divide' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(0)
+    homepage.getnumber1field().type(0)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(2)
     cy.get("#selectOperationDropdown").select("Divide")
@@ -216,15 +286,38 @@ it("Enter 0 divide it by integer number and test 'Divide' operation", ()=>{
     cy.get("#numberAnswerField").should('have.value', 0)
 })
 
-it("Enter integer number and divide by 0 by using 'Divide' operation", ()=>{
+it.only("Enter integer number and divide by 0 by using 'Divide' operation", ()=>{
     cy.get("#selectBuild").select(item)
     homepage.getnumber1field().clear()
-    homepage.getnumber1field().clear().type(3)
+    homepage.getnumber1field().type(3)
     homepage.getnumber2field().clear()
     homepage.getnumber2field().type(0)
     cy.get("#selectOperationDropdown").select("Divide")
     homepage.ClickClaculateButton()
     cy.get("#errorMsgField").contains('Divide by zero error!')
+})
+
+it("Enter decimal numbers and test 'Divide' operation", ()=>{
+    cy.get("#selectBuild").select(item)
+    homepage.getnumber1field().clear()
+    homepage.getnumber1field().type(3.14785474)
+    homepage.getnumber2field().clear()
+    homepage.getnumber2field().type(2.47758745)
+    cy.get("#selectOperationDropdown").select("Divide")
+    homepage.ClickClaculateButton()
+    cy.get("#numberAnswerField").should('have.value', 1.2705322429688606)
+})
+
+it.only("Enter decimal numbers use'Divide' operation and test Integers only functionality", ()=>{
+    cy.get("#selectBuild").select(item)
+    homepage.getnumber1field().clear()
+    homepage.getnumber1field().type(3.14785474)
+    homepage.getnumber2field().clear()
+    homepage.getnumber2field().type(2.47758745)
+    cy.get("#selectOperationDropdown").select("Divide")
+    homepage.ClickClaculateButton()
+    cy.get("#integerSelect").check()
+    cy.get("#numberAnswerField").should('have.value', 1)
 })
 
     })
