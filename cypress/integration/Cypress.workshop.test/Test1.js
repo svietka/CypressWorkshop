d9985afd3815966eee116f589ba837cfd53448ec
ErikasCep <ceponis.erikas@gmail.com>
2020-09-02 13:14:08 +0300

    no message

--- cypress/integration/Cypress.workshop.test/Test1.js
+++
@@ -1,9 +0,0 @@
-import homepage from '../../page.objects/landing.page/selector.js'
-
-/// <reference types="cypress" />
-
-it("Loads the duckduckGo page",()=>{
-    cy.visit("https://www.duckduckgo.com")
-    homepage.type("test").submit('#search_form_homepage')
-    cy.contains("Speedtest by Ookla")
-})
--- cypress/integration/Cypress.workshop.test/Test2.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-it("search for some stuff and close baner",()=>{
-    cy.visit("https://www.duckduckgo.com")
-    cy.get('#search_form_homepage').type("belekas seni").submit('#search_form_homepage')
-    cy.get('.js-badge-main-msg > .ddgsi').click()
-    cy.get('.badge-link__thumb__img').should('not.be.visible')
-})
-
