36baa222d50659830a5a6d5f5a74b6fce7edea71
Gediminas <gediminas.makseckas@gmail.com>
2020-09-02 18:18:49 +0300

    no message

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -4,7 +4,7 @@ beforeEach("Executes before each test",()=>{
     cy.visit("/BasicCalculator")
 })
 
-it("Check if first build with operation 'Add' works properly and error message is visible", ()=>{
+it("Check if in first build error message is visible", ()=>{
     cy.get('[name="selectBuild"]').select("1")
         .get('#number1Field').type('1a')
         .get('#number2Field').type(1)
@@ -31,17 +31,16 @@ it("Check if fourth build with function 'Multiply' works properly and 'Integers 
         .get('#integerSelect').should('not.be.disabled')        
 })
 
-it("Check if ninth build has 'Second number' field", ()=>{
+it.only("Check if ninth build has 'Second number' field", ()=>{
     cy.get('[name="selectBuild"]').select("9")
         .get('#number1Field').type('5')
-        .get('#number2Field').should('not.be.disabled')
         .get('#number2Field').type(5)
         .get('#selectOperationDropdown').select('1')
         .get('#calculateButton').click()
         
     })
 
-it.only("Check if ninth build has 'Calculate' button", ()=>{
+it("Check if ninth build has 'Calculate' button", ()=>{
     cy.get('[name="selectBuild"]').select("9")
         .get('#number1Field').type('6')
         //.get('#number2Field').type(6)
