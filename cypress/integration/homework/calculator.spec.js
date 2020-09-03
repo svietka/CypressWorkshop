22194c04dacf13573d0cf13fded8e867cdc9ec50
Mr. Farty <easteregg489@gmail.com>
2020-09-04 00:27:39 +0300

    Added action variable
    Added a variable for actions

--- cypress/integration/homework/calculator.spec.js
+++ cypress/integration/homework/calculator.spec.js
@@ -4,7 +4,6 @@ import HomePage from './classes/HomePage.js'
 
 var homePage = new HomePage
 
-var listTags = ['1', '2', '3', '4']
 var website = "https://testsheepnz.github.io/BasicCalculator";
 
 var numberField1 = "#number1Field";
@@ -12,32 +11,32 @@ var numberField2 = "#number2Field";
 var clearButton = "#clearButton";
 var buildDropDown = "#selectBuild";
 var actionDropDown = "#selectOperationDropdown";
-
+var action = "";
 
 describe('Calculator Homepage', () => {
     describe('Addition', () => {
         it.only("Test addition on every version of a calculator", () => {
-
+            action = "Add";
             cy.visit(website)
-            cy.get(actionDropDown).select("Add")
+            cy.get(actionDropDown).select(action)
             for (var i = 1; i <= 9; i++) {
                 context(`Build ${i}`, () => {
                     cy.get(buildDropDown).select(i.toString());
                     homePage.checkIfFieldsEnabled(numberField1)
                     homePage.checkIfFieldsEnabled(numberField2)
                     homePage.checkIfFieldsEnabled(clearButton)
-                    homePage.enterFieldsWith("-1", "-1", "Add")
+                    homePage.enterFieldsWith("-1", "-1", action)
                     homePage.checkAnswer('-2')
-                    homePage.enterFieldsWith("1", "1", "Add")
+                    homePage.enterFieldsWith("1", "1", action)
                     homePage.checkAnswer('2')
-                    homePage.enterFieldsWith("Ding", "Dong", "Add")
+                    homePage.enterFieldsWith("Ding", "Dong", action)
                     homePage.checkAnswer('NaN')
-                    homePage.enterFieldsWith("3006", "-300", "Add")
+                    homePage.enterFieldsWith("3006", "-300", action)
                     homePage.checkAnswer('2706')
-                    homePage.enterFieldsWith("2147483647", "1000", "Add")
+                    homePage.enterFieldsWith("2147483647", "1000", action)
                     homePage.checkAnswer('2147484647')
                     homePage.checkTestClearButton();
-                    homePage.TestIntegersOnlyCheckbox("Add");
+                    homePage.TestIntegersOnlyCheckbox(action);
                 })
             }
 
@@ -45,27 +44,27 @@ describe('Calculator Homepage', () => {
     })
     describe('Subtraction', () => {
         it.only("Test subtraction on every version of a calculator", () => {
-
+            action = "Subtract";
             cy.visit(website)
-            cy.get(actionDropDown).select("Subtract")
+            cy.get(actionDropDown).select(action)
             for (var i = 1; i <= 9; i++) {
                 context(`Build ${i}`, () => {
                     cy.get(buildDropDown).select(i.toString());
                     homePage.checkIfFieldsEnabled(numberField1)
                     homePage.checkIfFieldsEnabled(numberField2)
                     homePage.checkIfFieldsEnabled(clearButton)
-                    homePage.enterFieldsWith("-1", "-1", "Subtract")
+                    homePage.enterFieldsWith("-1", "-1", action)
                     homePage.checkAnswer('0')
-                    homePage.enterFieldsWith("1", "1", "Subtract")
+                    homePage.enterFieldsWith("1", "1", action)
                     homePage.checkAnswer('0')
-                    homePage.enterFieldsWith("Ding", "Dong", "Subtract")
+                    homePage.enterFieldsWith("Ding", "Dong", action)
                     homePage.checkAnswer('NaN')
-                    homePage.enterFieldsWith("3006", "-300", "Subtract")
+                    homePage.enterFieldsWith("3006", "-300", action)
                     homePage.checkAnswer('3306')
-                    homePage.enterFieldsWith("2147483647", "1000", "Subtract")
+                    homePage.enterFieldsWith("2147483647", "1000", action)
                     homePage.checkAnswer('2147482647')
                     homePage.checkTestClearButton();
-                    homePage.TestIntegersOnlyCheckbox("Subtract");
+                    homePage.TestIntegersOnlyCheckbox(action);
                 })
             }
 
@@ -73,27 +72,27 @@ describe('Calculator Homepage', () => {
     })
     describe('Multiply', () => {
         it.only("Test multiplication on every version of a calculator", () => {
-
+            action = "Multiply";
             cy.visit(website)
-            cy.get(actionDropDown).select("Multiply")
+            cy.get(actionDropDown).select(action)
             for (var i = 1; i <= 9; i++) {
                 context(`Build ${i}`, () => {
                     cy.get(buildDropDown).select(i.toString());
                     homePage.checkIfFieldsEnabled(numberField1)
                     homePage.checkIfFieldsEnabled(numberField2)
                     homePage.checkIfFieldsEnabled(clearButton)
-                    homePage.enterFieldsWith("-1", "-1", "Multiply")
+                    homePage.enterFieldsWith("-1", "-1", action)
                     homePage.checkAnswer('1')
-                    homePage.enterFieldsWith("1", "1", "Multiply")
+                    homePage.enterFieldsWith("1", "1", action)
                     homePage.checkAnswer('1')
-                    homePage.enterFieldsWith("Ding", "Dong", "Multiply")
+                    homePage.enterFieldsWith("Ding", "Dong", action)
                     homePage.checkAnswer('NaN')
-                    homePage.enterFieldsWith("3006", "-300", "Multiply")
+                    homePage.enterFieldsWith("3006", "-300", action)
                     homePage.checkAnswer('-901800')
-                    homePage.enterFieldsWith("2147483647", "1000", "Multiply")
+                    homePage.enterFieldsWith("2147483647", "1000", action)
                     homePage.checkAnswer('2147483647000')
                     homePage.checkTestClearButton();
-                    homePage.TestIntegersOnlyCheckbox("Multiply");
+                    homePage.TestIntegersOnlyCheckbox(action);
                 })
             }
 
@@ -101,27 +100,27 @@ describe('Calculator Homepage', () => {
     })
     describe('Divide', () => {
         it.only("Test division on every version of a calculator", () => {
-
+            action = "Divide";
             cy.visit(website)
-            cy.get(actionDropDown).select("Divide")
+            cy.get(actionDropDown).select(action)
             for (var i = 1; i <= 9; i++) {
                 context(`Build ${i}`, () => {
                     cy.get(buildDropDown).select(i.toString());
                     homePage.checkIfFieldsEnabled(numberField1)
                     homePage.checkIfFieldsEnabled(numberField2)
                     homePage.checkIfFieldsEnabled(clearButton)
-                    homePage.enterFieldsWith("-1", "-1", "Divide")
+                    homePage.enterFieldsWith("-1", "-1", action)
                     homePage.checkAnswer('1')
-                    homePage.enterFieldsWith("1", "1", "Divide")
+                    homePage.enterFieldsWith("1", "1", action)
                     homePage.checkAnswer('1')
-                    homePage.enterFieldsWith("Ding", "Dong", "Divide")
+                    homePage.enterFieldsWith("Ding", "Dong", action)
                     homePage.checkAnswer('NaN')
-                    homePage.enterFieldsWith("3006", "-300", "Divide")
+                    homePage.enterFieldsWith("3006", "-300", action)
                     homePage.checkAnswer('-10.02')
-                    homePage.enterFieldsWith("2147483647", "1000", "Divide")
+                    homePage.enterFieldsWith("2147483647", "1000", action)
                     homePage.checkAnswer('2147483.647')
                     homePage.checkTestClearButton();
-                    homePage.TestIntegersOnlyCheckbox("Divide");
+                    homePage.TestIntegersOnlyCheckbox(action);
                 })
             }
 
@@ -129,27 +128,27 @@ describe('Calculator Homepage', () => {
     })
     describe('Concatenate', () => {
         it.only("Test concatenation on every version of a calculator", () => {
-
+            action = "Concatenate";
             cy.visit(website)
-            cy.get(actionDropDown).select("Concatenate")
+            cy.get(actionDropDown).select(action)
             for (var i = 1; i <= 9; i++) {
                 context(`Build ${i}`, () => {
                     cy.get(buildDropDown).select(i.toString());
                     homePage.checkIfFieldsEnabled(numberField1)
                     homePage.checkIfFieldsEnabled(numberField2)
                     homePage.checkIfFieldsEnabled(clearButton)
-                    homePage.enterFieldsWith("-1", "-1", "Concatenate")
+                    homePage.enterFieldsWith("-1", "-1", action)
                     homePage.checkAnswer('-1-1')
-                    homePage.enterFieldsWith("1", "1", "Concatenate")
+                    homePage.enterFieldsWith("1", "1", action)
                     homePage.checkAnswer('11')
-                    homePage.enterFieldsWith("Ding", "Dong", "Concatenate")
+                    homePage.enterFieldsWith("Ding", "Dong", action)
                     homePage.checkAnswer('DingDong')
-                    homePage.enterFieldsWith("3006", "-300", "Concatenate")
+                    homePage.enterFieldsWith("3006", "-300", action)
                     homePage.checkAnswer('3006-300')
-                    homePage.enterFieldsWith("2147483647", "1000", "Concatenate")
+                    homePage.enterFieldsWith("2147483647", "1000", action)
                     homePage.checkAnswer('21474836471000')
                     homePage.checkTestClearButton();
-                    homePage.TestIntegersOnlyCheckbox("Concatenate");
+                    homePage.TestIntegersOnlyCheckbox(action);
                 })
             }
 
