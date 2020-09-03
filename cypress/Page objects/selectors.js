5c838402da83eb6edcfa391e1b433059f314d487
LAPTOP-HQ5OS2SS\laury <laurynas.vilc@gmail.com>
2020-09-03 12:25:02 +0300

    Made updates to calculator tests

--- cypress/Page objects/selectors.js
+++ cypress/Page objects/selectors.js
@@ -1,13 +1,44 @@
 class selectors{
-    searchbutton = '#search_button_homepage'
-    searchform = '#search_form_input_homepage'
+    BuildVersion = '#selectBuild'
+    Operations = '#selectOperationDropdown'
+    Number1Field = '#number1Field'
+    Number2Field = '#number2Field'
+    Answer = '#numberAnswerField'
+    CheckBox = '#integerSelect'
+    ClearButton = '#clearButton'
+    Calculate = '#calculateButton'
 
-    getsearchbutton(){
-        return cy.get(this.searchbutton)
+
+    getBuildVersion(){
+        return cy.get(this.BuildVersion)
     }
 
-    getSearchInputField(){
-        return cy.get(this.searchform)
+    getOperations(){
+        return cy.get(this.Operations)
+    }
+
+    getNumber1Field(){
+        return cy.get(this.Number1Field)
+    }
+
+    getNumber2Field(){
+        return cy.get(this.Number2Field)
+    }
+
+    getAnswerField(){
+        return cy.get(this.Answer)
+    }
+
+    getIntegerSelect(){
+        return cy.get(this.CheckBox)
+    }
+
+    getClearButton(){
+        return cy.get(this.ClearButton)
+    }
+    
+    getCalculateButton(){
+        return cy.get(this.Calculate)
     }
 }
 
--- cypress/integration/Calculator_Tests/tests.spec.js
+++ cypress/integration/Calculator_Tests/tests.spec.js
@@ -2,6 +2,7 @@ import selectors from '../../Page objects/selectors.js'
 /// <reference types="cypress" />
 
 const BuildVersions = ['0','1','2','3','4','5','6','7','8','9']
+const Operations = ['0','1','2','3','4']
 
 var selector = new selectors
 
@@ -9,80 +10,158 @@ beforeEach('Executes  before each test', () =>{
     cy.visit('https://testsheepnz.github.io/BasicCalculator')
 })
 
-it('Loads the calculator page', ()=>{
-    cy.contains("Basic Calculator")
-})
+// it('Loads the calculator page', ()=>{
+//     cy.contains("Basic Calculator")
+// })
 
-describe('selects calculator build version', () => {
+describe('tests every calculator build version buttons, forms, lists and checkboxes', () => {
     BuildVersions.forEach((version) => {
-        beforeEach('Executes before each test', () =>{
-            cy.get('#selectBuild').select(version)
+
+        // it('select version of calculator' + version, ()=>{
+        //     selector.getBuildVersion().select(version)
+        // })
+
+        it('checks if all dropdowns, forms and buttons are visible in build version ' + version, ()=>{
+            selector.getBuildVersion().select(version)
+            selector.getBuildVersion().should('be.visible')
+            selector.getNumber1Field().should('be.visible')
+            selector.getNumber2Field().should('be.visible')
+            selector.getOperations().should('be.visible')
+            selector.getCalculateButton().should('be.visible')
+            selector.getAnswerField().should('be.visible')
+            selector.getIntegerSelect().should('be.visible')
+            selector.getClearButton().should('be.visible')
         })
 
-        it('select version of calculator' + version, ()=>{
-            cy.get('#selectBuild').select(version)
-        })
-
-        it('checks if all dropdowns, forms and buttons are visible in CALCULATOR', ()=>{
-            cy.get('#selectBuild').should('be.visible')
-            cy.get('#number1Field').should('be.visible')
-            cy.get('#number2Field').should('be.visible')
-            cy.get('#selectOperationDropdown').should('be.visible')
-            cy.get('#calculateButton').should('be.visible')
-            cy.get('#numberAnswerField').should('be.visible')
-            cy.get('#integerSelect').should('be.visible')
-            cy.get('#clearButton').should('be.visible')
-        })
-
-        it('checks if all buttons or checkboxes are enabled', ()=>{
-            cy.get('#calculateButton').should('be.enabled')
-            cy.get('#integerSelect').should('not.be.disabled')
-            cy.get('#clearButton').should('be.enabled')
+        it('checks if all buttons or checkboxes are enabled in build version ' + version, ()=>{
+            selector.getBuildVersion().select(version)
+            selector.getCalculateButton().should('be.enabled')
+            selector.getIntegerSelect().should('not.be.disabled')
+            selector.getClearButton().should('be.enabled')
         })
     })
 })
 
-// it('Can display results relevent to search term and search for relevant results', ()=> {
-//     selector.getSearchInputField().type('Test')
-//     selector.getsearchbutton().click()
-//     cy.contains('test')
-// })
+describe('tests every calculator build version calculations', () => {
+    BuildVersions.forEach((version) => {
+        
+        describe('tests every calculation operation of build version ' + version, () =>{
+            Operations.forEach((operation) => {
 
-// it("Removes the banner when X is pressed", () => {
-//     selector.getSearchInputField().type("test")
-//     cy.get('form').submit();
-//     cy.get('.js-badge-main-msg > .ddgsi').click()
-//     cy.get('.badge-link__thumb__img').should("not.be.visible")
-// })
+                it('tests if error message apears when you try to calculate not only with numbers. Build: '+version+'; Operation: '+operation, ()=>{
+                    selector.getBuildVersion().select(version)
+                    selector.getOperations().select(operation)
+                    selector.getNumber1Field().type('2.4"%1fQx')
+                    selector.getCalculateButton().click()
+                    cy.get('#errorMsgField').should('be.visible')
+                })
 
-// it("intitle search options works 1", () => {
-//     selector.getSearchInputField().type("intitle:panda{enter}");
-//     cy.get(".result__title").each(($el) => {
-//         cy.wrap($el).contains("panda", {matchCase: false});
-//     });
-// })
+                it('tests if clear button works. Build: '+version+'; Operation: '+operation, ()=>{
+                    selector.getBuildVersion().select(version)
+                    selector.getOperations().select(operation)
+                    selector.getNumber1Field().type('2')
+                    selector.getNumber2Field().type('1')
+                    selector.getCalculateButton().click()
+                    selector.getClearButton().click()
+                    selector.getAnswerField().should('be.empty')
+                })
+            })
+        })
 
-// it("intitle search options works 2", () => {
-//     selector.getSearchInputField().type("intitle:panda{enter}")
-//     cy.get('.result__title').each(($item) => { 
-//         cy.get($item).contains('Panda', { matchCase: false })
-//     })
-// })
+        it('tests calculator build '+version+' and operation ADD, if integer checkbox works as expected', ()=>{
+            selector.getBuildVersion().select(version)
+            selector.getOperations().select('0')
+            selector.getIntegerSelect().should('not.be.disabled')
+            selector.getIntegerSelect().check()
+            selector.getNumber1Field().type('2.4')
+            selector.getNumber2Field().type('2.2')
+            selector.getCalculateButton().click()
+            selector.getAnswerField().contains('4')
+        })
 
-// it("should redirect to first result", ()=> {
-//     selector.getSearchInputField().type("!wiki")
-//     selector.getsearchbutton().click()
-// })
+        it('tests calculator build '+version+' and operation SUBTRACT, if integer checkbox works as expected', ()=>{
+            selector.getBuildVersion().select(version)
+            selector.getOperations().select('1')
+            selector.getIntegerSelect().should('not.be.disabled')
+            selector.getIntegerSelect().check()
+            selector.getNumber1Field().type('2.4')
+            selector.getNumber2Field().type('2.2')
+            selector.getCalculateButton().click()
+            selector.getAnswerField().contains('0')
+        })
 
-// describe('should generate secure passwords', () => {
-//     [8, 32, 64].forEach((passwordLenght) => {
-//     it.only('generates password with length: ' + passwordLenght, () => {
-//     cy.visit("www.duckduckgo.com")
-//     cy.get('#search_form_homepage').type("password " + passwordLenght)
-//     cy.get('#search_button_homepage').click();
-//     cy.get('.c-base__title').then(($title) => {
-//     cy.get($title).invoke('text').its('length').should('be.eq', passwordLenght)
-//     })
-//     })
-//     })
-//     })
+        it('tests calculator build '+version+' and operation MULTIPLY, if integer checkbox works as expected', ()=>{
+            selector.getBuildVersion().select(version)
+            selector.getOperations().select('2')
+            selector.getIntegerSelect().should('not.be.disabled')
+            selector.getIntegerSelect().check()
+            selector.getNumber1Field().type('2.4')
+            selector.getNumber2Field().type('2.2')
+            selector.getCalculateButton().click()
+            selector.getAnswerField().contains('5')
+        })
+
+        it('tests calculator build '+version+' and operation DIVIDE, if integer checkbox works as expected', ()=>{
+            selector.getBuildVersion().select(version)
+            selector.getOperations().select('3')
+            selector.getIntegerSelect().should('not.be.disabled')
+            selector.getIntegerSelect().check()
+            selector.getNumber1Field().type('2.4')
+            selector.getNumber2Field().type('2.2')
+            selector.getCalculateButton().click()
+            selector.getAnswerField().contains('1')
+        })
+        // There could be similar tests to the last four, to check if calculator calculates correctly and concatenate operation tests.
+        // Those tests will be below this comment (They don't have to be evaluated)
+
+        // it('tests calculator build '+version+' and operation ADD calculation', ()=>{
+        //     selector.getBuildVersion().select(version)
+        //     selector.getOperations().select('0')
+        //     selector.getIntegerSelect().uncheck()
+        //     selector.getNumber1Field().type('2.4')
+        //     selector.getNumber2Field().type('2.2')
+        //     selector.getCalculateButton().click()
+        //     selector.getAnswerField().contains('4.6')
+        // })
+
+        // it('tests calculator build '+version+' and operation SUBTRACT calculation', ()=>{
+        //     selector.getBuildVersion().select(version)
+        //     selector.getOperations().select('1')
+        //     selector.getIntegerSelect().uncheck()
+        //     selector.getNumber1Field().type('2.4')
+        //     selector.getNumber2Field().type('2.2')
+        //     selector.getCalculateButton().click()
+        //     selector.getAnswerField().contains('0.19999999999999973')
+        // })
+
+        // it('tests calculator build '+version+' and operation MULTIPLY calculation', ()=>{
+        //     selector.getBuildVersion().select(version)
+        //     selector.getOperations().select('2')
+        //     selector.getIntegerSelect().uncheck()
+        //     selector.getNumber1Field().type('2.4')
+        //     selector.getNumber2Field().type('2.2')
+        //     selector.getCalculateButton().click()
+        //     selector.getAnswerField().contains('5.28')
+        // })
+
+        // it('tests calculator build '+version+' and operation DIVIDE calculation', ()=>{
+        //     selector.getBuildVersion().select(version)
+        //     selector.getOperations().select('3')
+        //     selector.getIntegerSelect().uncheck()
+        //     selector.getNumber1Field().type('2.4')
+        //     selector.getNumber2Field().type('2.2')
+        //     selector.getCalculateButton().click()
+        //     selector.getAnswerField().contains('1.0909090909090908')
+        // })
+
+        // it('tests calculator build '+version+' and operation CONCATENATE calculation', ()=>{
+        //     selector.getBuildVersion().select(version)
+        //     selector.getOperations().select('4')
+        //     selector.getIntegerSelect().uncheck()
+        //     selector.getNumber1Field().type('2.4')
+        //     selector.getNumber2Field().type('2.2')
+        //     selector.getCalculateButton().click()
+        //     selector.getAnswerField().contains('2.42.2')
+        // })
+    })
+})
