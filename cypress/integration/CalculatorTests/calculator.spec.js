aead4b9f63c21cc41e42dac9ddbd5f34032741ae
zilvinas.cep <zilvinas.cep@gmail.com>
2020-09-03 16:35:36 +0300

    Add landing page POM, tests

--- cypress/integration/CalculatorTests/calculator.spec.js
+++ cypress/integration/CalculatorTests/calculator.spec.js
@@ -1,76 +1,247 @@
-import LandingPage from '../../page_objects/landingpage/landingpage.js'
+import LandingPage from "../../page_objects/landingpage/landingpage.js";
+import * as utils from "../../utility_functions/utils.js";
 /// <reference types="cypress" />
 
 let landingPage = new LandingPage();
-
-beforeEach('Executes before each test',()=>{
-    cy.visit('/BasicCalculator');
-    
-})
-
-
 const builds = [...Array(10).keys()].map(String);
-const operations = ['Add', 'Subtract', 'Multiply', 'Divide', 'Concatenate']
+const mathOperations = landingPage.mathOperationTitles;
+const randomNumbers = [
+  {
+    number: utils.generateRandomPositiveInt, //dataFunction
+    numberType: "positiveInteger"
+  },
+  {
+    number: utils.generateRandomNegativeInt,
+    numberType: "negativeInteger"
+  },
+  {
+    number: utils.generateRandomPositiveFloat,
+    numberType: "positiveFloat"
+  },
+  {
+    number: utils.generateRandomNegativeFloat,
+    numberType: "negativeFloat"
+  },
+  { number: () => 0, numberType: "zero" },
+  {
+    number: utils.generateRandomLetter,
+    numberType: "letter"
+  }
+];
 
-builds.forEach(build=>{
+beforeEach("", () => {
+  cy.visit("/BasicCalculator");
+});
 
-    
+builds.forEach(build => {
+  describe(`Build: ${build}`, () => {
+    it(`User can click 'Clear' button`, () => {
+      landingPage.getBuildDropdown().select(build);
+      landingPage.getClearButton().should("not.be.disabled");
+    });
 
+    it(`User can choose 'Integers only'`, () => {
+      landingPage.getBuildDropdown().select(build);
+      let ops = landingPage.getOperationsDropdown();
+      ops.each(op => {
+        if (op.value !== landingPage.concatTitle) {
+          ops.select(op.val());
+          landingPage.getIntegerSelect().should("not.be.disabled");
+        }
+      });
+    });
 
-    it(`User can enter first number in build ${build} `, ()=>{
+    describe("Inputs should only operate on digits in mathematical operations", () => {
+      it(`First number input should only operate on digits`, () => {
+        let seed = 100;
         landingPage.getBuildDropdown().select(build);
-        landingPage.getFirstNumberField().type(5).should('have.value', '5');
-        
-    
-    
-    })
-    
-    it(`User can enter second number in build ${build} `, ()=>{
+
+        landingPage.getFirstNumberField().type(utils.generateRandomLetter());
+        landingPage
+          .getSecondNumberField()
+          .type(utils.generateRandomPositiveInt(seed));
+
+        mathOperations.forEach(op => {
+          landingPage.getOperationsDropdown().select(op);
+          landingPage.getCalculateButton().click();
+          landingPage.getErrorMessageField().should("be.visible");
+        });
+      });
+
+      it(`Second number input should only operate on digits`, () => {
+        let seed = 100;
         landingPage.getBuildDropdown().select(build);
-        landingPage.getSecondNumberField().type(5).should('have.value', '5');
-        
-    })
 
-    it(`User can click 'Calculate' button in build ${build} `, ()=>{
-        landingPage.getBuildDropdown().select(build);
-        landingPage.getCalculateButton().should('not.be.disabled');
-        
-    })
+        landingPage
+          .getFirstNumberField()
+          .type(utils.generateRandomPositiveInt(seed));
+        landingPage.getSecondNumberField().type(utils.generateRandomLetter());
 
+        mathOperations.forEach(op => {
+          landingPage.getOperations().select(op);
+          console.log(op);
 
-    it(`User can click 'Clear' button in build ${build} `, ()=>{
-        landingPage.getBuildDropdown().select(build);
-        landingPage.getClearButton().should('not.be.disabled');
-        
-    })
+          landingPage.getCalculateButton().click();
+          landingPage.getErrorMessageField().should("be.visible");
+        });
+      });
+    });
 
-    it.only(`User can choose integers value answer in build ${build} only `, ()=>{
-        landingPage.getBuildDropdown().select(build);
-        let ops = landingPage.getOperations();
-        ops.each(op => {
-            ops.select(op.value);
-            landingPage.getIntegerSelect().should('not.be.disabled');
+    describe(`Should perform addition correctly`, () => {
+      randomNumbers.forEach(testCase => {
+        it.only(`Can perform addition correctly with ${testCase.numberType} `, () => {
+          testCase.numberType === "letter"
+          let seed = 100;
+          let precision = 5;
 
-        })
+          let firstNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
+          let secondNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
 
-       /* landingPage.getOperations().each(op => {
-            landingPage.get
-        })*/
+          let firstNumber = parseFloat(
+            testCase.number(seed).toPrecision(precision)
+          );
+          let secondNumber = parseFloat(
+            testCase.number(seed).toPrecision(precision)
+          );
 
-    })
+          let answer = firstNumber + secondNumber;
 
+          landingPage.getBuildDropdown().select(build);
+          landingPage.getFirstNumberField().type(firstNumber);
+          landingPage.getSecondNumberField().type(secondNumber);
+          landingPage.getOperationsDropdown().select("Add");
+          landingPage.getCalculateButton().click();
 
+          landingPage.getAnswerField().should("have.value", answer);
+        });
+      });
+    });
 
+    describe(`Should perform subtraction correctly`, () => {
+      randomNumbers.forEach(testCase => {
+        it.only(`Can perform subtraction correctly with ${testCase.numberType} `, () => {
+          let seed = 100;
+          let precision = 5;
 
+          let firstNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
+          let secondNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
 
+          let answer = firstNumber - secondNumber;
 
+          landingPage.getBuildDropdown().select(build);
+          landingPage.getFirstNumberField().type(firstNumber);
+          landingPage.getSecondNumberField().type(secondNumber);
+          landingPage.getOperationsDropdown().select("Subtract");
+          landingPage.getCalculateButton().click();
 
-})
+          landingPage.getAnswerField().should("have.value", answer);
+        });
+      });
+    });
 
+    describe(`Should perform multiplication correctly`, () => {
+      randomNumbers.forEach(testCase => {
+        it.only(`Can perform multiplication correctly with ${testCase.numberType} `, () => {
+          let seed = 100;
+          let precision = 5;
+          let firstNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
+          let secondNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
 
-   
+          let answer = firstNumber * secondNumber;
 
+          landingPage.getBuildDropdown().select(build);
+          landingPage.getFirstNumberField().type(firstNumber);
+          landingPage.getSecondNumberField().type(secondNumber);
+          landingPage.getOperationsDropdown().select("Multiply");
+          landingPage.getCalculateButton().click();
 
+          landingPage.getAnswerField().should("have.value", answer);
+        });
+      });
+    });
 
+    describe(`Should perform division correctly`, () => {
+      randomNumbers
+        //.concat({
+        // number: () => 0,
+        //numberType: "zero"
+        //})
+        .forEach(testCase => {
+          it.only(`Can perform division correctly with ${testCase.numberType} `, () => {
+            let seed = 100;
+            let precision = 5;
+            let firstNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
+          let secondNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
 
+            landingPage.getBuildDropdown().select(build);
+            landingPage.getFirstNumberField().type(firstNumber);
+            landingPage.getSecondNumberField().type(secondNumber);
+            landingPage.getOperationsDropdown().select("Divide");
+            landingPage.getCalculateButton().click();
 
+            if (secondNumber === 0) {
+              landingPage
+                .getErrorMessageField()
+                .contains(landingPage.divideByZeroError);
+            } else {
+              let answer = firstNumber / secondNumber;
+
+              landingPage.getAnswerField().should("have.value", answer);
+            }
+          });
+        });
+    });
+
+    describe(`Should perform concatenation correctly`, () => {
+      randomNumbers.forEach(testCase => {
+        it.only(`Can perform concat correctly with ${testCase.numberType} `, () => {
+          let seed = 100;
+          let precision = 5;
+          let firstNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
+          let secondNumber =
+            testCase.numberType === "letter"
+              ? testCase.number()
+              : parseFloat(testCase.number(seed).toPrecision(precision));
+
+          landingPage.getBuildDropdown().select(build);
+          landingPage.getFirstNumberField().type(firstNumber);
+          landingPage.getSecondNumberField().type(secondNumber);
+
+          let answer = firstNumber.toString().concat(secondNumber.toString());
+
+          landingPage.getOperationsDropdown().select(landingPage.concatTitle);
+          landingPage.getCalculateButton().click();
+
+          landingPage.getAnswerField().should("have.value", answer);
+        });
+      });
+    });
+  });
+});
--- cypress/page_objects/landingpage/landingpage.js
+++ cypress/page_objects/landingpage/landingpage.js
@@ -1,54 +1,71 @@
-class LandingPage{
-    firstNumberField = '#number1Field';
-    secondNumberField = '#number2Field';
-    buildDropdown = '#selectBuild';
-    calculateButton = '#calculateButton';
-    clearButton = '#clearButton';
-    operations = '#selectOperationDropdown';
-    integerSelect = '#integerSelect';
+class LandingPage {
+  firstNumberField = "#number1Field";
+  secondNumberField = "#number2Field";
+  buildDropdown = "#selectBuild";
+  calculateButton = "#calculateButton";
+  clearButton = "#clearButton";
+  operationsDropdown = "#selectOperationDropdown";
+  integerSelect = "#integerSelect";
+  answerField = "#numberAnswerField";
+  errorMsgField = "#errorMsgField";
+  concatTitle = "Concatenate";
+  divideByZeroError = "Divide by zero error!";
+  mathOperationTitles = ["Add", "Subtract", "Multiply", "Divide"];
 
-    getFirstNumberField(){
-        return cy.get(this.firstNumberField);
-    }
+  getFirstNumberField() {
+    return cy.get(this.firstNumberField);
+  }
 
-    getSecondNumberField(){
-        return cy.get(this.secondNumberField);
-    }
+  getSecondNumberField() {
+    return cy.get(this.secondNumberField);
+  }
 
-    getBuildDropdown(){
-        return cy.get(this.buildDropdown);
-    }
+  getBuildDropdown() {
+    return cy.get(this.buildDropdown);
+  }
 
-    getBuildDropdownOptions(){
-        return cy.get(this.buildDropdown).children();
-    }
+  getBuildDropdownOptions() {
+    return cy.get(this.buildDropdown).children();
+  }
 
-    getBuildDropdownOptions1(){
-        console.log(cy.document().getElementById(this.buildDropdown).getElementsByTagName('option'))
-         cy.document().getElementById(this.buildDropdown).getElementsByTagName('option').array.map(element =>{
-            return element.value();
-        })
-    }
+  getBuildDropdownOptions1() {
+    console.log(
+      cy
+        .document()
+        .getElementById(this.buildDropdown)
+        .getElementsByTagName("option")
+    );
+    cy.document()
+      .getElementById(this.buildDropdown)
+      .getElementsByTagName("option")
+      .array.map(element => {
+        return element.value();
+      });
+  }
 
-    getCalculateButton(){
-        return cy.get(this.calculateButton);    
+  getCalculateButton() {
+    return cy.get(this.calculateButton);
+  }
 
-    }
+  getClearButton() {
+    return cy.get(this.clearButton);
+  }
 
-    getClearButton(){
-        return cy.get(this.clearButton);    
+  getOperationsDropdown() {
+    return cy.get(this.operationsDropdown);
+  }
 
-    }
+  getIntegerSelect() {
+    return cy.get(this.integerSelect);
+  }
 
-    getOperations(){
-        return cy.get(this.operations).children;
-    }
+  getAnswerField() {
+    return cy.get(this.answerField);
+  }
 
-    getIntegerSelect(){
-        return cy.get(this.integerSelect);
-    }
-
-
+  getErrorMessageField() {
+    return cy.get(this.errorMsgField);
+  }
 }
 
 export default LandingPage;
