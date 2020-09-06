e857ea1720e59364cb793042d87f2a52778aea26
AudreBart <bartaseviciuteaudre@gmail.com>
2020-09-06 12:34:09 +0300

    Actions to improve tests and to get better code readability

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# CypressWorkshop
-TEstas Kurkarolis
+{
+
+}
--- cypress/integration/TestHomework.spec.js
+++ cypress/integration/TestHomework.spec.js
@@ -1,25 +1,29 @@
-import Calculator from '../../../pageObjects/calculator1/calculator1.js'
+import Calculator from "../../cypress/pageObjects/calculator1/calculator1"
 
 /// <reference types="cypress" />
 
-var calculator = new Calculator()
+var calculator = new Calculator();
 
 beforeEach("Executes before each test", () => {
-
     cy.visit("testsheepnz.github.io/BasicCalculator")
+    //cy.visit("/")
 })
 
+const num1 = Cypress._.random(-5000, 5000);
+const num2 = Cypress._.random(-5000, 5000);
+const num3 = 128.59756;
+const num4 = 455.000125;
+const myString1 = "TE";
+const myString2 = "st2";
+const add = num1 + num2;
+const subtract = num1 - num2;
+const multiply = num1 * num2;
+const divide = num1 / num2;
+const result = num3 + num4;
+const result2 = myString1 + myString2;
+const buildVersion = "4";
+
 describe("Arithmetical operations", () => {
-
-    const num1 = Cypress._.random(-5000, 5000)
-    const num2 = Cypress._.random(-5000, 5000)
-
-    const add = num1 + num2
-    const subtract = num1 - num2
-    const multiply = num1 * num2
-    const divide = num1 / num2
-
-    const buildVersion = "0"
 
     function verifyOperation(buildVersion, operationToChoose, expectedValue) {
         cy.get("#selectBuild").select(buildVersion)
@@ -46,95 +50,63 @@ describe("Arithmetical operations", () => {
 
 describe("Toggle function", () => {
 
-    const buildVersion = "0"
-
-
     it("Integer toggle should not exist when concatonate was chosen", () => {
-
-        cy.get("#selectBuild").select(buildVersion)
+        calculator.getBuild().select(buildVersion)
         calculator.getNum1Field().type("TE")
         calculator.getNum2Field().type("ST")
         calculator.getOperation().select("4")
         calculator.getCalculateButton().click()
         calculator.getAnswerField().should("not.be.visible")
-
     })
 
     it("Should toggle when answer as an integer value wanted", () => {
-
-        const num1 = 128.59756
-        const num2 = 455.000125
-        const result = num1 + num2
-
-        cy.get("#selectBuild").select(buildVersion)
-        calculator.getNum1Field().type(num1)
-        calculator.getNum2Field().type(num2)
+        calculator.getBuild().select(buildVersion)
+        calculator.getNum1Field().type(num3)
+        calculator.getNum2Field().type(num4)
         calculator.getOperation().select("0")
         calculator.getCalculateButton().click()
         calculator.getAnswerField().should("have.value", result)
         cy.get("#integerSelect").click()
         calculator.getAnswerField().should("have.value", 583)
-
     })
-
 })
 
 describe("Invalid inputs", () => {
 
-    const buildVersion = "0"
-
     it("Should show error message when non-numerical values are entered and mathematical function is selected", () => {
-
-        cy.get("#selectBuild").select(buildVersion)
+        calculator.getBuild().select(buildVersion)
         calculator.getNum1Field().type("TEst")
         calculator.getNum2Field().type("@@@###")
         calculator.getOperation().select("2")
         calculator.getCalculateButton().click()
         cy.get("#errorMsgField").contains("Number 1 is not a number")
-
     })
 
-
     it("Should show error message when integer is divided by zero", () => {
-
-        cy.get("#selectBuild").select(buildVersion)
+        calculator.getBuild().select(buildVersion)
         calculator.getNum1Field().type(Cypress._.random(-1000, 1000))
         calculator.getNum2Field().type("0")
         calculator.getOperation().select("3")
         calculator.getCalculateButton().click()
         cy.get("#errorMsgField").contains("Divide by zero error!")
-
     })
 })
 
-
 it("Should concatonate two strings", () => {
-
-    const myString1 = "TE"
-    const myString2 = "st2"
-    const result = myString1 + myString2
-
-    cy.get("#selectBuild").select("0")
+    calculator.getBuild().select("0")
     calculator.getNum1Field().type(myString1)
     calculator.getNum2Field().type(myString2)
     calculator.getOperation().select("4")
     calculator.getCalculateButton().click()
-    calculator.getAnswerField().should("have.value", result)
-
+    calculator.getAnswerField().should("have.value", result2)
 })
 
 it("Should show empty answer field when Clear button was clicked", () => {
-
-    cy.get("#selectBuild").select("0")
+    calculator.getBuild().select("0")
     calculator.getNum1Field().type("10")
     calculator.getNum2Field().type("5")
     calculator.getOperation().select("0")
     cy.get("#clearButton").click()
     calculator.getAnswerField().should("be.empty")
-    //last line of code could also be:
-    //cy.get("#numberAnswerField").should("have.value", "")
-
 })
 
-/*pakeitimas
-*/
