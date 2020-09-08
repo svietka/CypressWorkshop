c9e5b7c9884bfb4f82c55bd3cdc1064c17e6ab50
DESKTOP-8Q6BP98\Bellend <vytautas.mikalonis@gmail.com>
2020-09-08 16:17:08 +0100

    Mikalonis-Test2

--- cypress/integration/WorkshopTests/tests.spec.js
+++ cypress/integration/WorkshopTests/tests.spec.js
@@ -2,7 +2,9 @@
 import Homepage from '/Users/ieva/CypressWorkshop/cypress/page_objects/homepage/Homepage.js'
 
 var homepage = new Homepage();
-
+ 
+// amazing
+ 
 beforeEach("Executes before each test", ()=>{
     cy.visit("/");
 })
